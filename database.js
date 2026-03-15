c7e3bf6afb376efdf2bdb1ee47ab3e0b4ab52b7f
SiddheshAryan <saryan@student.gitam.edu>
2026-03-15 19:53:31 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,30 +1,39 @@
 const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database("./safety_tracker.db", (err) => {
-    if (err) {
-        console.error(err.message);
-    } else {
-        console.log("Connected to SQLite database");
-    }
-});
+const db = new sqlite3.Database("./safety_tracker.db",(err)=>{
+
+if(err){
+console.log(err.message)
+}
+else{
+console.log("Connected to SQLite database")
+}
+
+})
+
+/* USERS TABLE */
 
 db.run(`
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    roll TEXT UNIQUE,
-    password TEXT
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT,
+roll TEXT UNIQUE,
+parentName TEXT,
+phone TEXT,
+password TEXT
 )
-`);
+`)
+
+/* GPS TRACKING TABLE */
 
 db.run(`
 CREATE TABLE IF NOT EXISTS tracking (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    roll TEXT,
-    latitude REAL,
-    longitude REAL,
-    time TEXT
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+roll TEXT,
+latitude REAL,
+longitude REAL,
+time TEXT
 )
-`);
+`)
 
 module.exports = db;
